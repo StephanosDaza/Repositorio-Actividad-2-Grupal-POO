77889b3137dabab77e4f0d9e0ba89ff4064b2fc3
mn4n4m <147223809+mn4n4m@users.noreply.github.com>
2025-05-03 19:03:33 -0500

    Create Cuadrado.java

---
+++ Ejercicio 2.4/Cuadrado.java
@@ -0,0 +1,15 @@
+public class Cuadrado {
+    int lado;
+
+    Cuadrado(int lado) {
+        this.lado = lado;
+    }
+
+    double calcularArea() {
+        return lado * lado;
+    }
+
+    double calcularPerimetro() {
+        return 4 * lado;
+    }
+}
